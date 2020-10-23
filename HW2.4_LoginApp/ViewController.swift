aa11d584a80f5af47b44884bac2afe6ea74ad495
darik88 <mr.darik88@gmail.com>
2020-10-23 21:57:11 +0300

    Initial Commit

---
+++ HW2.4_LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4_LoginApp
+//
+//  Created by Айдар Рахматуллин on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
